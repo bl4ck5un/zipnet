90589874dc114f20661863eeb4d8bd1d0f04eca1
jasonzhao404 <1531186050@qq.com>
2023-10-10 15:26:30 +0000

    aggregator adding data-split function

--- aggregator/src/util.rs
+++ aggregator/src/util.rs
@@ -1,9 +1,10 @@
 use crate::agg_state::AggregatorState;
-
-use std::{fs::File, io};
-
 use common::{cli_util, enclave::EnclaveError};
+use interface::UserSubmissionMessage;
+use log::info;
+use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::{convert::TryInto, fs::File, io};
 use thiserror::Error;
 
 pub(crate) type Result<T> = core::result::Result<T, AggregatorError>;
@@ -24,7 +25,11 @@ pub enum AggregatorError {
 
 pub(crate) fn load_state(save_path: &str) -> Result<AggregatorState> {
     let save_file = File::open(save_path)?;
-    Ok(cli_util::load(save_file)?)
+    let mut loaded_state: AggregatorState = cli_util::load(save_file)?;
+    if loaded_state.agg_number.is_none() {
+        loaded_state.agg_number = Some(0);
+    }
+    Ok(loaded_state)
 }
 
 pub(crate) fn save_state(save_path: &str, state: &AggregatorState) -> Result<()> {
@@ -42,4 +47,47 @@ pub(crate) fn save_to_stdout<S: Serialize>(val: &S) -> Result<()> {
     cli_util::save(stdout, val)?;
     println!("");
     Ok(())
+}
+
+///[onlyevaluation]split the data_collection to several pieces, for multi-thread
+pub(crate) fn split_data_collection(num_user: u32, thread: u32) {
+    let save_path = "data_collection.txt";
+    let file = File::open(save_path).unwrap();
+    let data_collection_loaded: Vec<UserSubmissionMessage> = cli_util::load(file).unwrap();
+    info!("Data loaded from {}", save_path);
+
+    let remainder = num_user % thread;
+    let single_leaf_agg_msg_num = (num_user - remainder) / thread;
+    (0..remainder).into_par_iter().for_each(|i| {
+        let index_start = (i * (single_leaf_agg_msg_num + 1)).try_into().unwrap();
+        let index_end = ((i + 1) * (single_leaf_agg_msg_num + 1))
+            .try_into()
+            .unwrap();
+        let data_slice: &[UserSubmissionMessage] = &data_collection_loaded[index_start..index_end];
+        let data_vec: Vec<UserSubmissionMessage> = data_slice.to_vec();
+
+        let save_path_prefix = "data_collection_";
+        let save_path_postfix = ".txt";
+        let save_path = format!("{}{}{}", save_path_prefix, i + 1, save_path_postfix);
+        let file = std::fs::File::create(save_path.clone()).unwrap();
+        cli_util::save(file, &data_vec).unwrap();
+        info!("Data saved to {}", save_path);
+    });
+    (remainder..thread).into_par_iter().for_each(|i| {
+        let index_start = (i * single_leaf_agg_msg_num + remainder)
+            .try_into()
+            .unwrap();
+        let index_end = ((i + 1) * single_leaf_agg_msg_num + remainder)
+            .try_into()
+            .unwrap();
+        let data_slice: &[UserSubmissionMessage] = &data_collection_loaded[index_start..index_end];
+        let data_vec: Vec<UserSubmissionMessage> = data_slice.to_vec();
+
+        let save_path_prefix = "data_collection_";
+        let save_path_postfix = ".txt";
+        let save_path = format!("{}{}{}", save_path_prefix, i + 1, save_path_postfix);
+        let file = std::fs::File::create(save_path.clone()).unwrap();
+        cli_util::save(file, &data_vec).unwrap();
+        info!("Data saved to {}", save_path);
+    });
 }
