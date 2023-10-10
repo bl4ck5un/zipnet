3db05347601f55fc5beee69d2bcbf46eb949aa65
jasonzhao404 <1531186050@qq.com>
2023-10-10 15:32:48 +0000

    exposing docker's port to outside

--- start-docker-real-sgx.sh
+++ start-docker-real-sgx.sh
@@ -11,6 +11,8 @@ else
   docker run \
     -v $PWD:/root/sgx \
     -ti \
+    -p 18300:18300 \
+    -p 28942:28942 \
     --hostname $CONTAINER_NAME \
     --name $CONTAINER_NAME \
     -e SGX_MODE=HW \
